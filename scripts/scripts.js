1d01c45612474be0ca2961ada5b9587917858ff2
Saloni Anand <69853372+saloni05@users.noreply.github.com>
2023-03-18 01:29:37 +0530

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -38,7 +38,7 @@ function buildHeroBlock(main) {
 function buildPatientAccessBannerFragment(main) {
   if (document.querySelector('.prefooter-container')) return;
   const patientAccessPath = getMetadata('prefooter') || '/prefooter';
-console.log(hello);
+console.log('hello');
   console.log(patientAccessPath);
   const fragment = buildBlock('fragment', [[
     `<a href="${patientAccessPath}">${window.location.origin}${patientAccessPath}</a>`,
